bbd7c7f1a0a13e5712f9c45199f83b3e755640ea
llzhen... <llzhen61212@gmail.com>
2026-05-15 11:41:50 +0800

    Update and rename login.html to data.js

---
+++ data.js
@@ -0,0 +1,32 @@
+const benefits = [
+  {
+    group: "LE SSERAFIM",
+    album: "HOT",
+    version: "Compact Ver.",
+    store: "Weverse Shop Global",
+    type: "Photo Card",
+    image: "images/lesserafim-hot-wvs-global.jpg",
+    link: "https://weverse.io/",
+    note: "購買指定版本贈送，售完可能不補"
+  },
+  {
+    group: "LE SSERAFIM",
+    album: "HOT",
+    version: "Set Ver.",
+    store: "Weverse Shop Japan",
+    type: "Photo Card",
+    image: "images/lesserafim-hot-wvs-jp.jpg",
+    link: "https://weverseshop.io/",
+    note: "日本區特典，通常會標示售完為止"
+  },
+  {
+    group: "TOMORROW X TOGETHER",
+    album: "minisode 3: TOMORROW",
+    version: "Ethereal Ver.",
+    store: "Universal Music Japan",
+    type: "Postcard",
+    image: "images/txt-minisode3-umj.jpg",
+    link: "https://www.universal-music.co.jp/",
+    note: "日本通路特典"
+  }
+];

175f8166ef4a69662e67b8812eb09ddf05c7b749
llzhen... <llzhen61212@gmail.com>
2026-05-15 12:23:33 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,32 +1,32 @@
 const benefits = [
   {
     group: "LE SSERAFIM",
-    album: "HOT",
-    version: "Compact Ver.",
+    album: "‘PUREFLOW’ pt.1",
+    version: "Set ver.",
     store: "Weverse Shop Global",
-    type: "Photo Card",
-    image: "images/lesserafim-hot-wvs-global.jpg",
-    link: "https://weverse.io/",
-    note: "購買指定版本贈送，售完可能不補"
+    type: "小卡+紙質支架",
+    image: "images/LSF正二wvsPB版.png",
+    link: "https://share.weverseshop.io/static/shares/sale/57273",
+    note: "預售到5/21 22：59"
   },
   {
     group: "LE SSERAFIM",
-    album: "HOT",
-    version: "Set Ver.",
-    store: "Weverse Shop Japan",
-    type: "Photo Card",
-    image: "images/lesserafim-hot-wvs-jp.jpg",
-    link: "https://weverseshop.io/",
-    note: "日本區特典，通常會標示售完為止"
+    album: "‘PUREFLOW’ pt.1",
+    version: "COMPACT ver.",
+    store: "Weverse Shop Global",
+    type: "小卡+專輯盒",
+    image: "images/LSF正二wvs單封版 png",
+    link: "https://share.weverseshop.io/static/shares/sale/57275",
+    note: "預售到5/21 22：59"
   },
   {
-    group: "TOMORROW X TOGETHER",
-    album: "minisode 3: TOMORROW",
-    version: "Ethereal Ver.",
-    store: "Universal Music Japan",
-    type: "Postcard",
-    image: "images/txt-minisode3-umj.jpg",
-    link: "https://www.universal-music.co.jp/",
-    note: "日本通路特典"
+    group: "LE SSERAFIM",
+    album: "‘PUREFLOW’ pt.1",
+    version: "Weverse Albums ver.",
+    store: "Weverse Shop Global",
+    type: "手持鏡",
+    image: "images/LSF正二wvs電子專版.png",
+    link: "https://share.weverseshop.io/static/shares/sale/57277",
+    note: "預售到5/21 22：59"
   }
 ];
